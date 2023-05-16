c49214fcc5f6a51fa9f0f815177049c003146700
bidyutsamanta293 <bidyut.samanta@propelius.tech>
2023-05-16 12:04:27 +0530

    wip with mock-questiondata

--- src/components/sections/welcome/index.tsx
+++ src/components/sections/welcome/index.tsx
@@ -18,9 +18,9 @@ const Welcome = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+  // const handleBack = () => {
+  //   setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  // };
 
   return (
     <>
@@ -44,6 +44,7 @@ const Welcome = () => {
                 maxWidth: 400,
                 flexGrow: 1,
                 mt: 3,
+                margin: "auto",
                 ".MuiMobileStepper-progress": {
                   width: "100%",
                   borderRadius: "5px",
@@ -66,6 +67,38 @@ const Welcome = () => {
               <Box sx={{ width: { xs: "100%", md: "50%" } }}>
                 <Option />
               </Box>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: { lg: "end", xs: "start" },
+                paddingRight: { lg: "33px", xs: "0px" },
+                paddingLeft: { lg: "0px", xs: "14px" },
+              }}
+            >
+              <Button
+                size="small"
+                variant="contained"
+                sx={{
+                  width: { xs: "30%", lg: "10%" },
+                  fontSize: {
+                    xs: "14px",
+                    lg: "18px",
+                  },
+                  fontWeight: "bold",
+                  height: "fit-content",
+                  padding: { xs: "7px", lg: "5px" },
+                }}
+                onClick={handleNext}
+                disabled={activeStep === 5}
+              >
+                Next
+                {theme.direction === "rtl" ? (
+                  <KeyboardArrowLeft />
+                ) : (
+                  <KeyboardArrowRight />
+                )}
+              </Button>
             </Box>
           </Box>
         </Paper>
---
+++ src/mockData/mockQuestionData.ts
@@ -0,0 +1,85 @@
+export const mockQuestionData = [
+  {
+    question:
+      "Which of the following is used in React.js to increase performance?",
+    option: {
+      1: "Virtual DOM",
+      2: "Original DOM",
+      3: "Both A and B",
+      4: "None of the above",
+    },
+    type: "mcq",
+  },
+  {
+    question: " Which of the following is the correct name of React.js?",
+    option: {
+      1: "React",
+      2: "React.js",
+      3: "ReactJS",
+      4: "All of the above",
+    },
+    type: "mcq",
+  },
+  {
+    question: "Which of the following are the advantages of React.js?",
+    option: {
+      1: "React.js can increase the application's performance with Virtual DOM.",
+      2: "React.js is easy to integrate with other frameworks such as Angular, BackboneJS since it is only a view library.",
+      3: "React.js can render both on client and server side.",
+      4: "All of the above",
+    },
+    type: "mcq",
+  },
+  {
+    question:
+      "Which of the following command is used to install create-react-app?",
+    option: {
+      1: "npm install -g create-react-app",
+      2: "npm install create-react-app",
+      3: "npm install -f create-react-app",
+      4: "install -g create-react-app",
+    },
+    type: "mcq",
+  },
+  {
+    question:
+      "A class is a type of function, but instead of using the keyword function to initiate it, which keyword do we use?",
+    option: {
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+    },
+    type: "mcq" || "descriptive",
+  },
+  {
+    question: "",
+    option: {
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+    },
+    type: "mcq" || "descriptive",
+  },
+  {
+    question: "",
+    option: {
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+    },
+    type: "mcq" || "descriptive",
+  },
+  {
+    question: "",
+    option: {
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+    },
+    type: "mcq" || "descriptive",
+  },
+];
