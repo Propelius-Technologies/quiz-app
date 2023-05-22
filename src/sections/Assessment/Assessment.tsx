688d5198ee1ce0b19a2ede55da401cf7d5237f6e
Siddharth Vaishnav <siddharth.vaishnav@propelius.tech>
2023-05-22 10:28:26 +0530

    seperate function for getStepContent

--- src/sections/Assessment/Assessment.tsx
+++ src/sections/Assessment/Assessment.tsx
@@ -49,6 +49,85 @@ interface AssessmentProps {
   handleNext: () => void;
 }
 
+const getStepContent = ({
+  queData,
+  activeStep,
+  handleNext,
+  selectedQue,
+}: AssessmentProps) => {
+  return (
+    <Grid container spacing={2} sx={InnerContainerstyles}>
+      <Grid item xs={12} sx={leftAnswerContainerStyle}>
+        <Typography variant="h6" sx={LeftAnswerTextStyles}>
+          6 ANSWERS LEFT
+        </Typography>
+        <Box sx={TimeRemainingContainerStyles}>
+          <Typography variant="h6" sx={TimeRemainingTextStyles}>
+            Time Remaining
+          </Typography>
+          <CircularStatic activeStep={activeStep} />
+        </Box>
+      </Grid>
+
+      <Grid item xs={12} sx={QuestionContainerStyles}>
+        <Typography variant="h6" sx={QuestionTextStyles}>
+          <span>{`${selectedQue + 1})`}</span>
+          {queData?.question}
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12} sx={OptionGridContainerStyle}>
+        <FormControl sx={OptionFormControlStyle}>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+            onChange={(e) => console.log(e.target.value)}
+          >
+            {queData?.option && (
+              <>
+                {Object.entries(queData.option).map((data, index) => {
+                  return (
+                    <>
+                      <FormControlLabel
+                        value={data[1]}
+                        key={index}
+                        control={
+                          <Radio
+                            sx={{ color: "#71748b59" }}
+                            checkedIcon={<CheckCircleIcon />}
+                          />
+                        }
+                        label={data[1]}
+                        sx={FormControlLabelStyle}
+                      />
+                    </>
+                  );
+                })}
+              </>
+            )}
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} sx={customButtonContainer}>
+        {activeStep === 95 ? (
+          <CustomButton
+            onclick={handleNext}
+            label="submit"
+            activeStep={activeStep}
+          />
+        ) : (
+          <CustomButton
+            onclick={handleNext}
+            label="Next question"
+            activeStep={activeStep}
+          />
+        )}
+      </Grid>
+    </Grid>
+  );
+};
+
 const Assessment: React.FC<AssessmentProps> = ({
   queData,
   selectedQue,
@@ -90,77 +169,7 @@ const Assessment: React.FC<AssessmentProps> = ({
           </Grid>
 
           <Divider sx={DividerStyles} />
-
-          <Grid container spacing={2} sx={InnerContainerstyles}>
-            <Grid item xs={12} sx={leftAnswerContainerStyle}>
-              <Typography variant="h6" sx={LeftAnswerTextStyles}>
-                6 ANSWERS LEFT
-              </Typography>
-              <Box sx={TimeRemainingContainerStyles}>
-                <Typography variant="h6" sx={TimeRemainingTextStyles}>
-                  Time Remaining
-                </Typography>
-                <CircularStatic activeStep={activeStep} />
-              </Box>
-            </Grid>
-
-            <Grid item xs={12} sx={QuestionContainerStyles}>
-              <Typography variant="h6" sx={QuestionTextStyles}>
-                <span>{`${selectedQue + 1})`}</span>
-                {queData?.question}
-              </Typography>
-            </Grid>
-
-            <Grid item xs={12} sx={OptionGridContainerStyle}>
-              <FormControl sx={OptionFormControlStyle}>
-                <RadioGroup
-                  aria-labelledby="demo-radio-buttons-group-label"
-                  defaultValue="female"
-                  name="radio-buttons-group"
-                  onChange={(e) => console.log(e.target.value)}
-                >
-                  {queData?.option && (
-                    <>
-                      {Object.entries(queData.option).map((data, index) => {
-                      
-                        return (
-                          <>
-                            <FormControlLabel
-                              value={data[1]}
-                              key={index}
-                              control={
-                                <Radio
-                                  sx={{ color: "#71748b59" }}
-                                  checkedIcon={<CheckCircleIcon />}
-                                />
-                              }
-                              label={data[1]}
-                              sx={FormControlLabelStyle}
-                            />
-                          </>
-                        );
-                      })}
-                    </>
-                  )}
-                </RadioGroup>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} sx={customButtonContainer}>
-              {activeStep === 95 ? (
-                <CustomButton
-                  onclick={handleNext}
-                  label="submit"
-                  activeStep={activeStep}
-                />
-              ) : (
-                <CustomButton
-                  onclick={handleNext}
-                  label="Next question"
-                  activeStep={activeStep}
-                />
-              )}
-            </Grid>
-          </Grid>
+          {getStepContent({ queData, activeStep, handleNext, selectedQue })}
         </Paper>
       </Box>
     </>
