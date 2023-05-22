70e49415efe3f4a2c8daad1b0e32139b9c7d265e
bidyutsamanta293 <bidyut.samanta@propelius.tech>
2023-05-22 17:57:17 +0530

    Refactor code [WIP]

--- src/data/MockData.ts
+++ src/data/MockData.ts
@@ -229,6 +229,11 @@ export const testData = [
   { id: 2, questions: mockQuestionData },
 ];
 
+export const testData = [
+  { id: 1, questions: mockQuestionData },
+  { id: 2, questions: mockQuestionData },
+];
+
 // Que-Ans Flow
 
 // {
