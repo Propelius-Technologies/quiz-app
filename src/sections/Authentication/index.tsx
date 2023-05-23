e61d441206c15ff738e44cfec47263c20ae23786
monika <monika.tiwari@propelius.tech>
2023-05-23 12:31:42 +0530

    change style file of Authentication ui

--- src/sections/Authentication/index.tsx
+++ src/sections/Authentication/index.tsx
@@ -2,6 +2,7 @@ import { Box, Typography} from "@mui/material";
 import GOOGLE_ICON from '@/src/assets/svgs/GoogleIcon.svg'
 import Image from "next/image";
 import OutlineButton from "@/src/components/common/CustomButton/OutlineButton";
+import {Button, MainBox, SignIN, WelcomeBox, WelcomeMessage} from "@/src/sections/Authentication/style";
 
 interface AuthenticationProps{
     handleGoogleAuth : () => void,
@@ -11,35 +12,14 @@ interface AuthenticationProps{
 const Authentication = ({handleGoogleAuth,isLoading}:AuthenticationProps) =>{
     return(
         <>
-         <Box sx={{backgroundColor:'#181B32',width:'100%',height:'100vh'}}>
-            <Box sx={{ display:'flex',
-                flexDirection:'column',
-                gap: {sm:'20px',xs:'30px'},
-                justifyContent:'center',
-                alignItems:'center',
-                padding:'20px',
-                width:'100%',height:'100%'}}>
-                <Typography sx={{
-                    fontFamily:'sans-serif',
-                    fontSize: {md:'56px',sm:'36px',xs:'26px'},
-                    fontWeight:'700',
-                    color:'#FFF',textAlign:'center'
-                }}>
+         <Box sx={MainBox}>
+            <Box sx={WelcomeBox}>
+                <Typography sx={WelcomeMessage}>
                     Welcome To Propelius <br/> Technologies
                 </Typography>
              
-                <Typography sx={{
-                    fontFamily:'sans-serif',
-                    fontSize: {sm:'20px',xs:'16px'},
-                    fontWeight: '500',
-                    color: '#FFF',
-                    textAlign: 'center'}}>Please sign in with your provided email to get started</Typography>
-                <Box
-                    sx={{
-                        width: { xs: "237px" },
-                        marginTop: {sm:"40px",xs:'15px'}
-                    }}
-                >
+                <Typography sx={SignIN}>Please sign in with your provided email to get started</Typography>
+                <Box sx={Button}>
                     <OutlineButton
                         loading={isLoading}
                         onClick={handleGoogleAuth}
---
+++ src/sections/Authentication/style.ts
@@ -0,0 +1,38 @@
+export const MainBox= {
+    backgroundColor:'#181B32',width:'100%',height:'100vh'
+}
+
+export const WelcomeBox = {
+     display:'flex',
+    flexDirection:'column',
+    gap: {sm:'20px',xs:'30px'},
+    justifyContent:'center',
+        alignItems:'center',
+        padding:'20px',
+        width:'100%',height:'100%'
+}
+
+export const WelcomeMessage ={
+   
+    fontFamily:'sans-serif',
+        fontSize: {md:'56px',sm:'36px',xs:'26px'},
+    fontWeight:'700',
+        color:'#FFF',textAlign:'center'
+
+}
+
+export const SignIN ={
+  
+    fontFamily:'sans-serif',
+        fontSize: {sm:'20px',xs:'16px'},
+    fontWeight: '500',
+        color: '#FFF',
+        textAlign: 'center'
+}
+
+export const Button ={
+   
+    width: { xs: "237px" },
+    marginTop: {sm:"40px",xs:'15px'}
+
+}
