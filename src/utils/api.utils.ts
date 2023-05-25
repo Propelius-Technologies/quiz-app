19a93f7265fc099e4db56a2e0e504b64de812bbc
bidyutsamanta293 <bidyut.samanta@propelius.tech>
2023-05-25 18:00:54 +0530

    api Integration authentication done

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,56 +1,59 @@
-import axios, { AxiosRequestConfig } from 'axios';
-import merge from 'lodash/merge';
-import { getAccessToken } from './token.utils';
+import axios, { AxiosRequestConfig } from "axios";
+import merge from "lodash/merge";
+import { getAccessToken } from "./token.utils";
 
-
-const API_PREFIX = `/api`;
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_HOST;
+const API_PREFIX = `/v1/auth`;
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+console.log({});
 
 export const AxiosInstance = axios.create({
-    baseURL: API_BASE_URL + API_PREFIX,
+  baseURL: API_BASE_URL + API_PREFIX,
 });
 
 export const fetch = async (config: AxiosRequestConfig) => {
-    // TODO remove this
-    const result: any = {
-        data: null,
-        error: null,
+  // TODO remove this
+  const result: any = {
+    data: null,
+    error: null,
+  };
+
+  try {
+    const response = await AxiosInstance(config);
+    result.data = response.data;
+  } catch (err) {
+    const { message, statusCode, error, branding } = (err as any).response.data;
+    const { status } = (err as any).response;
+
+    result.error = {
+      statusCode: statusCode || status,
+      message: message || String(error),
+      branding,
     };
+  }
 
-    try {
-        const response = await AxiosInstance(config);
-        result.data = response.data;
-    } catch (err) {
-        const { message, statusCode, error, branding } = (err as any).response.data;
-        const { status } = (err as any).response;
-
-        result.error = {
-            statusCode: statusCode || status,
-            message: message || String(error),
-            branding,
-        };
-    }
-
-    return result;
+  return result;
 };
 
 export const fetchAction = async (config: AxiosRequestConfig) => {
-    const token = getAccessToken();
+  const token = getAccessToken();
 
-    const specificConfig = {
-        headers: { Authorization: `Bearer ${token}` || '' },
-    };
-    const axconfig = merge({}, config, specificConfig);
-    const res = await fetch(axconfig);
-    const { data, error } = res;
+  const specificConfig = {
+    headers: { Authorization: `Bearer ${token}` || "" },
+  };
+  const axconfig = merge({}, config);
+  const res = await fetch(axconfig);
+  const { data, error } = res;
 
-    // TODO remove this
-    const result: any = { data, error };
+//   console.log({data})
+//   console.log({error})
 
-    if (error) {
-    }
+  // TODO remove this
+  const result: any = { data, error };
 
-    return result;
+  if (error) {
+  }
+
+  return data;
 };
 
 export default AxiosInstance;
--- src/zustand-store/candidate/candidate.slice.ts
+++ src/zustand-store/candidate/candidate.slice.ts
@@ -1,10 +1,68 @@
+import { fetchAction } from "@/src/utils/api.utils";
 import { AppStateCreator } from "../types";
 
-export interface CandidateSlice {}
+// export interface CandidateSlice {
+//   data: {
+//     id: number;
+//     uid: string;
+//     firstName: string;
+//     lastName: string;
+//     email: string;
+//     contactNumber: string;
+//     testUrl: string;
+//     createdAt: string;
+//     updatedAt: string;
+//   };
+// }
 
-const createCandidateSlice: AppStateCreator<CandidateSlice> = (
-  set,
-  get
-) => ({});
+export interface CandidateSlice {
+  userData: any;
+  fetchApplicant: (data: any) => void;
+  setUserData: (value: string | undefined) => void;
+  getAccesstoken: (data: any) => void;
+}
+
+export interface Data {
+  id: number;
+  uid: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  contactNumber: string;
+  testUrl: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// console.log("url", window.location.pathname);
+// let data = {
+//   candidateId: "8d41cfa5-b894-4067-9339-00054751d826",
+// };
+
+// export const fetchDataType = {
+//   // data: any;
+// };
+
+const createCandidateSlice: AppStateCreator<CandidateSlice> = (set, get) => ({
+  userData: [],
+  fetchApplicant: async (data: any) => {
+    const res = await fetchAction({
+      url: "/candidates/login",
+      method: "POST",
+      data,
+    });
+    // console.log({ res });
+    return await res.data;
+  },
+  setUserData: (value) => set((state) => ({ ...state, userData: value })),
+  getAccesstoken: async (data: any) => {
+    const res = await fetchAction({
+      url: "/candidates/login",
+      method: "POST",
+      data,
+    });
+    return await res.data;
+  },
+});
 
 export default createCandidateSlice;
