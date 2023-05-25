50c592557a84d27747af9ba7b1a9e289c6354c52
bidyutsamanta293 <bidyut.samanta@propelius.tech>
2023-05-25 18:00:54 +0530

    api Integration authentication done

--- src/pages/User/[userID].tsx
+++ src/pages/User/[userID].tsx
@@ -1,8 +1,10 @@
 import Authentication from "@/src/sections/Authentication";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { fireBaseApp } from "../../../firebase.config";
+import { GetServerSideProps } from "next";
+import useStore from "@/src/zustand-store";
 
 interface GoogleAuthProps {
   handleGoogleAuth: () => void;
@@ -12,6 +14,29 @@ const GoogleAuth = () => {
   const provider = new GoogleAuthProvider();
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
+  const fetchApplicant = useStore((state) => state.fetchApplicant);
+  const setUserData = useStore((state) => state.setUserData);
+
+  const getAccesstoken = useStore((state) => state.getAccesstoken);
+
+  const userData = useStore((state) => state.userData);
+
+  let Bodydata = {
+    candidateId: "8d41cfa5-b894-4067-9339-00054751d826",
+  };
+
+  const getUserData = async () => {
+    let data: any = await fetchApplicant(Bodydata);
+    setUserData(data);
+  };
+
+  // const getUserTokenData = async () => {
+  //   let tokenData: any = await getAccesstoken(data);
+  // };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
 
   const handleGoogleAuth = async () => {
     setIsLoading(true);
@@ -23,14 +48,28 @@ const GoogleAuth = () => {
     if (response.code && response.code === "auth/popup-closed-by-user") {
       setIsLoading(false);
     } else {
-      if (response.user?.email === "monika.tiwari@propelius.tech") {
+      if (response.user?.email === userData?.email) {
         setIsLoading(false);
-        router.push("/");
+        console.log("true email");
+
+        let data = {
+          candidateId: "8d41cfa5-b894-4067-9339-00054751d826",
+          guid: response.user.uid,
+          token: await response.user.getIdToken(),
+        };
+
+        let tokenData: any = await getAccesstoken(data);
+
+        console.log("tokenData", tokenData);
+
+        // router.push("/user");
       } else {
         console.log("Invalid email id");
+        console.log("response", await response.user.getIdToken());
       }
     }
   };
+  console.log("fetchApplicant", userData);
 
   return (
     <>
@@ -41,5 +80,13 @@ const GoogleAuth = () => {
     </>
   );
 };
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const selectedMovieData = await getMovieApi();
+
+//   return {
+//     props: { movie: selectedMovieData },
+//   };
+// };
 
 export default GoogleAuth;
